ac8d4840dbd2a83550c11299a89a8d741e5b44e3
Alexis Leclercq <slakenote@gmail.com>
2019-10-02 18:32:27 +0200

    ajout exo 1.2.1

---
+++ exo2/1.2.1.js
@@ -0,0 +1,19 @@
+function facto (a) {
+    nbre = parseInt(a);
+    let result = 1
+    let nbreOP = 0;
+
+    for (let i = a; i > 1; i--) {
+        result *= i;
+        nbreOP++;
+    }
+
+    console.log(`Factorielle de ${a} = ${result} (effectué en ${nbreOP} opérations)`);
+}
+
+facto(0);
+facto(3);
+facto("4");
+facto("5");
+facto(8);
+facto(10);
