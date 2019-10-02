b732ad36cdf192c1bde72a969f01f0e8ad684f8b
Alexis Leclercq <slakenote@gmail.com>
2019-10-02 13:27:19 +0200

    Ajout des commentaires dans exo 1.1.1 & 1.1.2 Modif sur le while du fichier 1.1.2.js ligne 35 pour éviter le double affichage de la solution si estimation == solution dès la première occurence

--- exo1/1.1.1.js
+++ exo1/1.1.1.js
@@ -1,11 +1,15 @@
+// Initialisation des variables
 let unNombre = tirerNombre();
 let saisie = 0;
 let prenom = prompt('Bonjour, quel est ton prénom ?');
 
+// Renvoit un nombre entier aléatoire entre 1 et 100 inclus
 function tirerNombre() {
   return parseInt(Math.random() * 100 + 1, 10);
 }
 
+// Test si la saisie de l'utilisateur est > ou < au nombre tiré aléatoirement et lui retourne une indication
+// s'il est proche ou très proche du résultat
 function supOuInf(a, b) {
   if (a == b) {
     alert(`Félicitations ${prenom}, le nombre à trouver était : ${unNombre} !`);
@@ -16,6 +20,7 @@ function supOuInf(a, b) {
 return "Au revoir";
 }
 
+// Renvoit des infos si on est proche (<=5) ou très proche (<= 6-10) du résultat
 function pasLoin(a, b) {
   if (Math.abs(b-a) <= 5 || Math.abs(a-b) <= 5) {
     return "mais vous êtes très proche";
@@ -25,7 +30,8 @@ function pasLoin(a, b) {
 }
 
 
-
+// Boucle qui demande de saisir un nombre entre 1 et 100, retourne une erreur dans le cas contraire
+// Si l'utilisateur a saisit un nombre entre 1 et 100, affiche le résultat de la fonction supOuInf()
 while (saisie != unNombre) {
   saisie = Number(prompt("Devinez un nombre entre 1 et 100 (inclus):"));
   if (isNaN(saisie) || saisie < 1 || saisie > 100) {
--- exo1/1.1.2.js
+++ exo1/1.1.2.js
@@ -1,8 +1,10 @@
+// Initialisation des variables
 let estimation = 50;
 let solution = Number(prompt('Bonjour, veuillez saisir un nombre entre 1 et 100'));
 let borneMin = 0, borneMax = 100;
 
-
+/* Vérification si l'estimation est égal à la solution, si oui alors on affiche la bonne réponse, si non nous affichons notre proposition
+   et nous lançons une nouvelle recherche */
 function verification() {
   if (solution == estimation) {
     alert(`La réponse était : ${solution}`);
@@ -12,6 +14,7 @@ function verification() {
     }
   }
 
+// Test si estimation est < ou > à la solution, puis ajustement des bornes min et max pour que le prochain test soit plus optimisé
 function recherche () {
   if (estimation < solution) {
     borneMin = estimation;
@@ -22,11 +25,14 @@ function recherche () {
   }
 }
 
+// Tant que solution n'est pas soit un nombre soit une valeur entre 1 et 100 nous affichons une erreur
 while (isNaN(solution) || solution < 1 || solution > 100) {
   solution = Number(prompt('Erreur: vous devez saisir un nombre entre 1 et 100 (inclus)'));
   }
 
-do {
+/* On appel verification() tant que estimation est différent de solution, une fois que c'est le cas on appel verification()
+pour afficher la réponse*/
+while (estimation != solution) {
   verification();
-} while (estimation != solution);
+}
 verification();
