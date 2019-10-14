8b319fec3df182075fe03dac185cdf0ae9d2bf7d
Alexis Leclercq <slakenote@gmail.com>
2019-10-14 17:32:54 +0200

    ajout exo 1.4.5 + modif 1.4.4

---
+++ exo1/1.4.5.js
@@ -0,0 +1,49 @@
+// Exercice 1.4.5 : Calculatrice en notation polonaise inversée
+
+let log = console.log;
+
+const calculer = (value) => {
+
+	let calculs = value.split(' ');
+	let stack = [];
+
+	for (let i = 0; i < calculs.length; i++) {
+
+		if (calculs[i] == '+' || calculs[i] == '-' || calculs[i] == '/' || calculs[i] == '*') {
+			
+			let a,b;
+
+			switch (calculs[i]) {
+				case '+':
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b + a);
+					break;
+				case '-':
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b - a);
+					break;
+				case '*':
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b * a);
+					break;
+				case '/':
+					a = stack.pop();
+					b = stack.pop();
+					stack.push(b / a);
+					break;
+			}
+		} 
+		else stack.push(Number(calculs[i]));
+	}
+	log(`${value} = ${stack}`);
+}
+
+
+log(calculer('12 4 - 2 *'));
+log('');
+log(calculer('3 4 1 2 + * +'));
+log('');
+log(calculer('1 2 + 4 * 3 +'));
