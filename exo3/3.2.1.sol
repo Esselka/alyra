851cee6bfcc952e80590cd0bc0fe5095262d7450
Alexis Leclercq <slakenote@gmail.com>
2019-10-24 17:01:25 +0200

    modif exo 3.2.1

--- exo3/3.2.1.sol
+++ exo3/3.2.1.sol
@@ -1,28 +1,23 @@
+pragma solidity ^0.5.11;
+
 contract CagnotteFestival {
     
-    Organisateur organisateurs;
+    mapping (address => uint) organisateurs;
     
     constructor() public {
-        organisateurs.parts[msg.sender] = 100;
-        organisateurs.isOrga[msg.sender] = true;
+        organisateurs[msg.sender] = 100;
     }
     
-    struct Organisateur {
-        mapping (address => uint) parts;
-        address[] adresses;
-        mapping (address => bool) isOrga;
-    }
-    
-    function estOrga(address orga) public returns (bool){
-        return organisateurs.isOrga[orga];
+    function estOrga(address orga) public view returns (bool){
+        // Si l'adresse 'orga' possède au moins 1 part, alors c'est un organisateur
+        return organisateurs[orga] > 0 ? true : false;
     }
     
     function transfererOrga(address orga, uint parts) public {
         require(estOrga(msg.sender), "Vous devez être organisateur pour effectuer cette opération");
-        //require(estOrga(orga), "La personne à qui vous voulez transférer des parts doit être un organisateur");
-        require(organisateurs.parts[msg.sender] > parts, "Vous essayez de transférer plus de parts que vous n'en possédez");
+        require(organisateurs[msg.sender] >= parts, "Vous essayez de transférer plus de parts que vous n'en possédez");
         
-        organisateurs.parts[orga] += parts;
-        organisateurs.parts[msg.sender] -= parts;
+        organisateurs[orga] += parts;
+        organisateurs[msg.sender] -= parts;
     }
 }
