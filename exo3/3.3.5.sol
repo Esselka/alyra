2a4b3aa96e1d7dd379fe5275352a32dd381a52bc
Alexis Leclercq <slakenote@gmail.com>
2019-10-27 23:06:51 +0100

    ajout exo 3.3.5

---
+++ exo3/3.3.5.sol
@@ -0,0 +1,54 @@
+pragma solidity ^0.5.11;
+
+contract Pulsation {
+    
+    uint public battement;
+    string private message;
+    uint public nombre;
+    
+    constructor(string memory unMessage) public {
+        battement = 0;
+        message = unMessage;
+    }
+    
+    function ajouterBattement() public returns (string memory) {
+        battement++;
+        
+        // Renvoie "Criii" 10% du temps au lieu du message
+        return random10Pourcent() >= 10 ? message : "Criii";
+    }
+    
+    function random10Pourcent () public returns (uint) {
+        nombre = (uint(blockhash(block.number-1)))%99;
+        return nombre;
+    }
+}
+
+contract Pendule  {
+    
+    Pulsation contratTic;
+    Pulsation contratTac;
+    string[] public balancier;
+    
+    function ajouterTicTac() public {
+        contratTic = new Pulsation("Tic");
+        contratTac = new Pulsation("Tac");
+    }
+    
+    function mouvementsBalancier(uint k) public {
+        for(uint i = 0 ; i < k ; i++) {
+            balancier.push(contratTic.ajouterBattement());
+            balancier.push(contratTac.ajouterBattement());
+        }
+    }
+    
+    function compareStringsbyBytes(string memory s1, string memory s2) internal pure returns(bool) {
+        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
+    
+    function inspection() public view returns (uint indexErreur) {
+        for(uint i = 0 ; i < balancier.length; i++) {
+            return compareStringsbyBytes(balancier[i], "Criii") ? i : 55250; // 55250 est mon code pour dire qu'il n'y a pas d'erreur ici
+        }
+    }
+}
