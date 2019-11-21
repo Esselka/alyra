2cb278b40207eec5b1ffcf7581544669240bf85b
Alexis Leclercq <slakenote@gmail.com>
2019-11-22 00:56:14 +0100

    ajout exo 7.1.2

---
+++ exo 7/7.1.2.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch");
+
+function getLastExchangePrice (symbol) {
+    fetch('https://api.bitfinex.com/v1/pubticker/' + symbol)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log('Prix du dernier échange sur Bitfinex : ', myJson.last_price + '$');
+        })
+        .catch(err => console.error(err));
+}
+
+getLastExchangePrice('btceur')
