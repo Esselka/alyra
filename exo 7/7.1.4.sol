91be5b2f6a07e2685f4e74ab77b66d3e54ef3014
Alexis Leclercq <slakenote@gmail.com>
2019-11-23 17:40:05 +0100

    maj 7.1.4

--- exo 7/7.1.4.sol
+++ exo 7/7.1.4.sol
@@ -1,16 +1,14 @@
 pragma solidity ^0.5.11;
 
-import "github.com/Arachnid/solidity-stringutils/strings.sol";
+pragma experimental ABIEncoderV2;
 
 contract ERC721Simple {
-    
-    using strings for *;
     
     mapping (uint => address) tokens;
     mapping (address => uint) tokensCounter;
     
     event Transfer(address indexed _from, address indexed _to, uint256 _tokenId);
-
+    
     function balanceOf(address _owner) public view returns (uint256 balance) {
         require(_owner != address(0), "Veuillez rentrer une adresse valide.");
         return tokensCounter[_owner];
@@ -37,20 +35,23 @@ contract ERC721Simple {
 
 contract ObjetsMagiques is ERC721Simple {
     
-    function decrireObjet(uint256 _tokenId) public view returns (string memory Objet) {
-        require(exists(_tokenId),"Cet objet n'existe pas.");
-        
-        string memory rareteObj;  // 1er chiffre du token
-        string memory typeObj;    // 2ème chiffre du token
-        string memory puissanceObj; // 3ème chiffre du token
-        string memory modeleObj;  // 4ème chiffre du token
-        
-        rareteObj = _tokenId/1000 == 0 ? "commun" : _tokenId/1000 == 1 ? "rare" : _tokenId/1000 == 2 ? "divin" : "ERREUR";
-        typeObj = _tokenId%1000/100 == 0 ? "épée" : _tokenId%1000/100 == 1 ? "baguette" : _tokenId%1000/100 == 2 ? "arc" : "ERREUR";
-        puissanceObj = _tokenId%100/10 == 0 ? "5" : _tokenId%100/10 == 1 ? "10" : _tokenId%100/10 == 2 ? "15" : _tokenId%100/10 == 3 ? "25" : "ERREUR";
-        modeleObj = _tokenId%10 == 0 ? "Elfe" : _tokenId%10 == 1 ? "Nain" : _tokenId%10 == 2 ? "Humain" : _tokenId%10 == 3 ? "Orc" : "ERREUR";
-        
-        return string("Cet objet est un(e) ", typeObj, "fds");
+    struct Objet {
+        string rareteObj;  // 1er chiffre du token
+        string typeObj;    // 2ème chiffre du token
+        string puissanceObj; // 3ème chiffre du token
+        string modeleObj;  // 4ème chiffre du token
     }
     
+    function decrireObjet(uint256 _tokenId) public view returns (Objet memory unObjet) {
+        require(exists(_tokenId),"Cet objet n'existe pas.");
+        
+        Objet memory monObjet;
+        
+        monObjet.rareteObj = _tokenId/1000 == 0 ? "commun" : _tokenId/1000 == 1 ? "rare" : _tokenId/1000 == 2 ? "divin" : "ERREUR";
+        monObjet.typeObj = _tokenId%1000/100 == 0 ? "épée" : _tokenId%1000/100 == 1 ? "baguette" : _tokenId%1000/100 == 2 ? "arc" : "ERREUR";
+        monObjet.puissanceObj = _tokenId%100/10 == 0 ? "5" : _tokenId%100/10 == 1 ? "10" : _tokenId%100/10 == 2 ? "15" : _tokenId%100/10 == 3 ? "25" : "ERREUR";
+        monObjet.modeleObj = _tokenId%10 == 0 ? " Elfe" : _tokenId%10 == 1 ? " Nain" : _tokenId%10 == 2 ? " Humain" : _tokenId%10 == 3 ? "Orc" : " ERREUR";
+        
+        return monObjet; // retourne l'objet pour être exploité dans une interface web par la suite
+    }
 }
