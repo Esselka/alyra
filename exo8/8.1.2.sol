420ca76c7e7bf9bb5fa6e516b6ad03f2550d50ef
Alexis Leclercq <slakenote@gmail.com>
2019-12-16 15:03:20 +0100

    ajout exo 8.1.2

---
+++ exo8/8.1.2.sol
@@ -0,0 +1,30 @@
+pragma solidity ^ 0.5.0;
+
+contract CanalDePaiement {
+    enum EtatCanal { VIDE, ACTIF, ENCOURSFERMETURE, FERME }
+    
+    address partieA;
+    address partieB;
+    uint montant;
+    EtatCanal etat;
+    uint blocFermeture; 
+    uint dernierNonce;
+    uint equilibreA;
+    uint equilibreB;
+    
+    constructor(address _partieA, address _partieB, uint _montant) public {
+        partieA = _partieA;
+        partieB = _partieB;
+        montant = _montant;
+    }
+    
+    function financer() payable public {
+        require(msg.value >= montant, "Montant trop faible.");
+        require(msg.sender == partieA || msg.sender == partieB, "Vous n'êtes pas autorisé à utiliser ce canal de paiement.");
+        
+        if(msg.sender == partieA) equilibreA = msg.value;
+        if(msg.sender == partieB) equilibreB = msg.value;
+        
+        if(equilibreA >= montant && equilibreB >= montant) etat = EtatCanal.ACTIF;
+    }
+}
